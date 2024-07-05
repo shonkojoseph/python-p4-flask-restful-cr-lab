a3e47e5190e10afbc9ab04b9eee7101fcdc22312
Joseph <josephshonko99@gmail.com>
2024-07-05 12:29:10 +0300

    commiting cr-restful

--- server/models.py
+++ server/models.py
@@ -7,3 +7,9 @@ class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(200), nullable=True)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+
+    def __repr__(self):
+        return f'<Plant {self.name}>'
